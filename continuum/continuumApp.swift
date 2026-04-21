81bdf2f9ba6525ff583b61686188a27446929ad6
William Huang <72718676+imaginaryg00se@users.noreply.github.com>
2026-04-21 17:00:54 -0400

    Initial Commit

---
+++ continuum/continuumApp.swift
@@ -0,0 +1,17 @@
+//
+//  continuumApp.swift
+//  continuum
+//
+//  Created by William Huang on 4/21/26.
+//
+
+import SwiftUI
+
+@main
+struct continuumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
