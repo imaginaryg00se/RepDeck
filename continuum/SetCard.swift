85ae51ae40a64e472b0ee751a2eb0d8f95ad5555
William Huang <72718676+imaginaryg00se@users.noreply.github.com>
2026-04-21 17:16:20 -0400

    Created SetCard View

---
+++ continuum/SetCard.swift
@@ -0,0 +1,18 @@
+//
+//  SetCard.swift
+//  continuum
+//
+//  Created by William Huang on 4/21/26.
+//
+
+import SwiftUI
+
+struct SetCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SetCard()
+}
